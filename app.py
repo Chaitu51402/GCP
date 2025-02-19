51b7b2703d92cb3fe0b5037a738ed04ebe61faf2
Chaitu51402 <34308720+Chaitu51402@users.noreply.github.com>
2025-02-20 01:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Simple Flask application to test in production Part3'
+    return 'Hello, Simple Flask application to test in production Part4'
